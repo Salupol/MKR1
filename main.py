adc5cee6f425b960fd795a2b568a8d44e58ecee1
Salupol <y.pavel.t@gmail.com>
2023-10-17 13:47:42 +0300

    2

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import random
+
 n = int(input("Введіть розмірність масиву:"))
-array = [[random.randint(0,3) for _ in range(n)] for _ in range(n)]
-print("Початковий масив")
+while True:
+    try:
+        a = int(input("Введіть нижню межу чисел в масиві:"))
+        b = int(input("Введіть верхню межу чисел в масиві:")) # розкоментувати для вибору верхньої та верхньої межі чисел
+        break
+    except ValueError:
+        print("Введені некоректні числа, спробуйте ще раз")
+array = [[random.randint(a, b) for _ in range(n)] for _ in range(n)]  # генеруемо масив
+print("Початковий масив:")
 for row in array:
-    print(row)
+    print(row)  # виводимо початковий масив
 column_sums = [0] * n
 for i in range(n):
     for row in array:
-        column_sums[i] += row[i]
+        column_sums[i] += row[i]  # рахуемо суму колонок масиву
 print("\nСуми для кожної колонки:")
-print(column_sums)
-sorted_columns = sorted(range(n), key=lambda x: column_sums[x])
-sorted_array = [[row[i] for i in sorted_columns] for row in array]
+print(column_sums)  # виводимо
+sorted_columns = sorted(range(n),
+                        key=lambda x: column_sums[x])  # використовуємо лямдба-функцію як критерій сотрування колонок
+sorted_array = [[row[i] for i in sorted_columns] for row in array]  # сортуемо колонки по сумі
 
 print("\nВідсортований масив:")
 for row in sorted_array:
-    print(row)
+    print(row)  # виводимо на екран
